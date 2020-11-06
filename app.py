9724094bfb5713e013c254845136db7eed79f265
Taylor Browne <taylorchristianbrowne@gmail.com>
2020-11-06 09:39:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ st.sidebar.subheader('How is the valuation calculated?')
 st.sidebar.write('The **Clean-Surplus-Model (Risidual Income Model)** is used to determining market value, utilizing balance sheet and income statement fundamentals. The two variables hard to define in this model are Return on Equity (RoE) and the horizon in which RoE exceeds Cost of Capital (Kc). Therefore, we leverage a monte carlo simulation over a uniform probability for input variables to determine the possible range of valuations')
 
 st.sidebar.subheader('Assumptions')
-st.sidebar.write('Assumes capital market conditions are stable and dividend irrelevancy.')
+st.write('Assumes capital market conditions are stable and dividend irrelevancy.')
 st.sidebar.write('Assumes that the company is not in a growth stage (i.e., negative ROE). Might be more appropriate to use projected or industry average ROE.')
 st.sidebar.write('Assumes that ROE stays constant for horizon input. In reality, ROE can change in response to a number of factors such as competition. ROE has been determined from the fundamentals of the financial statements in one particular year. Therefore, it is static and may not encapsulate all the information gathered / researched, for instances, by analysts.')
 st.sidebar.write('Additionally, model assumes that abnormal earnings persist for the horizon given and then drop to zero. Reality could be quite different, for example, persist for only three years or fifteen. There is potential for a "declining abnormal earnings pattern" that is not captured in this model.')
