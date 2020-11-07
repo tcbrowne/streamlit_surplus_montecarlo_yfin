05b206751a8e3e43c5e6b842265aa83a646d6f7d
TAYLOR BROWNE <Taylorchristianbrowne@gmail.com>
2020-11-07 01:06:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -119,15 +119,15 @@ def crude_monte_carlo(num_samples):
 
     return (Value_var,roe_len_Var,roe_var) #float(sum_of_samples/num_samples)
 
-#Streamlit Section for WebApp
-st.sidebar.subheader('Variables pulled from yahoo finance')
-st.sidebar.markdown('Net Income: {}'.format(byNI))
-st.sidebar.markdown('Number of Shares: {}'.format(numshares))
-st.sidebar.markdown('Current Year Book Value: {}'.format(byBV))
-st.sidebar.markdown('Prior Year Book Value: {}'.format(pyBV))
-st.sidebar.markdown('Dividends Paid: {}'.format(byDiv))
-st.sidebar.markdown('Beta: {}'.format(Bta))
+st.sidebar.subheader('Financial Statement Variables for {}'.format(option))
+st.sidebar.write('Net Income: {}'.format(byNI))
+st.sidebar.write('Number of Shares: {}'.format(numshares))
+st.sidebar.write('Current Year Book Value: {}'.format(byBV))
+st.sidebar.write('Prior Year Book Value: {}'.format(pyBV))
+st.sidebar.write('Dividends Paid: {}'.format(byDiv))
+st.sidebar.write('Beta: {}'.format(Bta))
 
+#Streamlit Section for WebApp
 st.sidebar.subheader('How is the valuation calculated?')
 st.sidebar.markdown('The **Clean-Surplus-Model (Risidual Income Model)** is used to determining market value, utilizing balance sheet and income statement fundamentals. The two variables hard to define in this model are Return on Equity (RoE) and the horizon in which RoE exceeds Cost of Capital (Kc). Therefore, we leverage a monte carlo simulation over a uniform probability for input variables to determine the possible range of valuations')
 
@@ -140,9 +140,9 @@ st.sidebar.markdown('Additionally, model assumes that abnormal earnings persist 
 st.title('Monte Carlo: Share Price of {}'.format(option))
 
 # sim1 = st.slider('How many simulations would you like to run?',100,100000,1000)
-sim1 = st.select_slider(
+sim1 = st.radio(
      'How many simulations would you like to run?',
-     options=[100, 1000, 10000, 100000, 1000000])
+     (options=100, 1000, 10000, 100000, 1000000))
 
 st.subheader("Variable #1: Length of expected earnings surprise (RoE > cost of capital).")
 
